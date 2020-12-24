fe80de86ca8a9b7f738a567a0ea87387afbbae4c
RakuJa <danielegiachetto1999@gmail.com>
2020-12-24 16:30:04 +0100

    Completed and working login

---
+++ php/login.php
@@ -0,0 +1,105 @@
+<?php
+    require_once('connessione.php');
+    require_once('debugger.php');
+    require_once('sessione.php');
+    //$_POST['ID_Viewer'],$_POST['ID_Recensione']
+
+    if($_SESSION['logged']==true){
+        new Debugger("User already logged in");
+        header('location:index.php');
+        exit();
+    }
+    new Debugger("User not logged in");
+
+    if(isset($_COOKIE['user_email'])){
+        $email=$_COOKIE['user_email'];
+        $check='checked="checked"';
+    }else{
+        $email='';
+        $check='';
+    }
+
+    if(isset($_COOKIE['user_pwd'])){
+        $pwd=$_COOKIE['user_pwd'];
+    }else{
+        $pwd='';
+    }
+
+    new Debugger("Cookies checked");
+
+    $error='';
+
+    // se ci sono valori in _POST cerca di fare il login o stampa errore
+
+    if(isset($_POST['email'])){
+        new Debugger("Found email on post");
+        $email=$_POST['email'];
+        new Debugger("Email is ".$email);
+        if(isset($_POST['pwd'])){
+            $pwd=$_POST['pwd'];
+            new Debugger("Found password on post");
+            new Debugger("Password is ".$pwd);
+        }
+        /*
+        if(isset($_POST['remember_me'])){
+            $check='checked="checked"';
+        }
+        */
+        new Debugger("Trying connection with database...");
+        $connection_established = False;
+        $obj_connection = new DBConnection();
+        $connection_established = $obj_connection->create_connection();
+        if($connection_established){
+            new Debugger("Connessione con il DB instaurata");
+            $email=$obj_connection->escape_str(trim($email));
+            //$hashed_pwd=hash("sha256",$obj_connection->escape_str(trim($pwd)));
+            $pwd = $obj_connection->escape_str(trim($pwd));
+            $query = "SELECT * FROM User WHERE EMAIL = '$email' AND PASSWORD = '$pwd'";
+            if($query_rist=$obj_connection->connessione->query($query)){
+                $array_rist=$obj_connection->queryToArray($query_rist);
+                $count=0;
+                foreach ($array_rist as &$value) {
+                    $count=$count+1;
+                }
+                new Debugger($count);
+                if($count==0){
+                    $error="[Le credenziali inserite non sono corrette]";
+                }else{
+                    $_SESSION['logged']=true;
+                    $_SESSION['EMAIL']=$email;
+                    $_SESSION['ID']=$log_array[0]['ID'];
+                    $_SESSION['PERMISSION']=$log_array[0]['Permessi'];
+                    new Debugger("Logged in!");
+                    if(isset($_POST['remember_me'])){
+                        setcookie("user_email",$email,time()+60*60*24*30);
+                        setcookie("user_pwd",$pwd,time()+60*60*24*30); 
+                        new Debugger("Logged for a long time");   
+                    }else{
+                        setcookie("user_email",$email,time()-3600);
+                        setcookie("user_pwd",$pwd,time()-3600); 
+                        new Debugger("Logged for a short while");
+                    }
+
+                    $obj_connection->close_connection();
+                
+                    header('location: index.php');
+                    exit;
+                }
+            }else {
+                $error="[La query non è andata a buon fine]";
+            }
+            $obj_connection->close_connection();
+
+        }else{
+            new Debugger("Connessione con il DB fallita");
+            $error=(new errore('DBConnection'))->printHTMLerror();
+        }
+
+    }else {
+        $error = "nessun dato trovato dentro url";
+    }
+    new Debugger($error);
+
+    echo $page;
+    echo "kekw";
+?>
