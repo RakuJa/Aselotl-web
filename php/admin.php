dcbcad4f835a00840f892aa497e7995bbe45c70c
Dema00 <dema199900@gmail.com>
2021-01-03 00:53:08 +0100

    fixato css pagine errore

--- php/admin.php
+++ php/admin.php
@@ -2,6 +2,6 @@
     session_start();
     $page_head = readfile("../html/head.html");
     include 'header.php';
-    $page_body = readfile("../html/add_fanart.html");
+    $page_body = readfile("../html/admin.html");
     $page_footer = readfile("../html/footer.html");
 ?>
