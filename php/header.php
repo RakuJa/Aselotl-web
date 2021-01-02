394755de7a2963105d14c8ef7007754ece730851
TheAsel <theasel99@gmail.com>
2021-01-02 16:39:15 +0100

    riaggiunti i fix precedenti

--- php/header.php
+++ php/header.php
@@ -1,12 +1,27 @@
 <div id="header">
 	<a href="index.php" class="logo"><img id="logo" src="../img/logo_small.png" alt="Immagine stilizzata della faccia di un axolotl sorridente sorridente" /> <h1>Axolotl Society</h1><br clear="all" /></a>
 	<div class="header-right">
-		<?php if (isset($_SESSION['logged']) && $_SESSION['logged']==true) { ?>
-			<a href="../php/profile.php" class="colored"> Profile </a>
-            <a href="../php/logout.php" class="colored">Logout</a>
-        <?php } else { ?>
-            <a href="../php/login.php" class="colored">Accedi</a>
-            <a href="../php/register.php" class="colored">Registrati</a>
+		<?php 
+		$protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+		$url = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+		if (isset($_SESSION['logged']) && $_SESSION['logged']==true) {
+			if($url == "http://127.0.0.1/php/profile.php") { ?>
+				<p> Profilo </p>
+			<?php } else { ?>
+				<a href="../php/profile.php" class="colored"> Profilo </a>
+			<?php } ?>
+		<a href="../php/logout.php" class="colored">Logout</a>
+		<?php } else {
+			if($url == "http://127.0.0.1/php/login.php") { ?>
+				<p> Accedi </p>
+			<?php } else { ?>
+				<a href="../php/login.php" class="colored"> Accedi </a>
+			<?php }
+			if($url == "http://127.0.0.1/php/register.php") { ?>
+				<p> Registrati </p>
+			<?php } else { ?>
+				<a href="../php/register.php" class="colored"> Registrati </a>
+			<?php } ?>
         <?php } ?>
 	</div>
 </div>
