9a7a361dd8b7f85bd28037ad196f9224ffccc662
TheAsel <theasel99@gmail.com>
2021-01-03 00:56:44 +0100

    aggiunto redirect per admin e edit_profile

--- php/admin.php
+++ php/admin.php
@@ -1,5 +1,8 @@
 <?php
     session_start();
+	if ($_SESSION['logged']==false) {
+		header("location: ../php/access_denied.php");
+	}
     $page_head = readfile("../html/head.html");
     include 'header.php';
     $page_body = readfile("../html/admin.html");
