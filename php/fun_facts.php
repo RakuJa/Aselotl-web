716a6fb54cc7e3dea95b751ffe1032224f7ec763
RakuJa <danielegiachetto1999@gmail.com>
2021-01-02 15:46:08 +0100

    Fix fun facts borked & remove useless file

--- php/fun_facts.php
+++ php/fun_facts.php
@@ -1,24 +1,7 @@
 ﻿<?php
     session_start();
     $page_head = readfile("../html/head.html");
+    include('header.php');
+    $page_body = readfile("../html/fun_facts.html");
+    $page_footer = readfile("../html/footer.html");
 ?>
-
-    <body>
-        <div id="header">
-            <a href="index.php" class="logo"><img id="logo" src="../img/logo_small.png" alt="Immagine stilizzata della faccia di un axolotl sorridente sorridente" /> <h1>Axolotl Society</h1><br clear="all" /></a>
-            <div class="header-right">
-              <?php if (isset($_SESSION['logged']) && $_SESSION['logged']==true) { ?>
-                    <a href="../php/profile.php" class="colored"> Profile </a>
-                    <a href="../php/logout.php" class="colored">Logout</a>
-                <?php } else { ?>
-                    <a href="../php/login.php" class="colored">Accedi</a>
-                    <a href="../php/register.php" class="colored">Registrati</a>
-                <?php } ?>
-            </div>
-          </div>
-
-
-        <?php
-            $page_body = readfile("../html/fun_facts.html");
-            $page_footer = readfile("../html/footer.html");
-        ?>
