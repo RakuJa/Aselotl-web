d730ee3a84c63302a76f8268ab402b54f5a1805c
RakuJa <danielegiachetto1999@gmail.com>
2021-01-02 15:39:58 +0100

    Fixed fan art not working

--- php/fanart.php
+++ php/fanart.php
@@ -2,6 +2,6 @@
     session_start();
     $page_head = readfile("../html/head.html");
     include 'header.php';
-    $page_body = readfile("../html/famous.html");
+    $page_body = readfile("../html/fanart.html");
     $page_footer = readfile("../html/footer.html");
 ?>
