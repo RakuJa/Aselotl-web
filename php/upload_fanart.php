14156e957a42acd831c5b89f1d4f12c23dfceea2
RakuJa <danielegiachetto1999@gmail.com>
2021-01-02 17:33:59 +0100

    Fix borked upload image

--- php/upload_fanart.php
+++ php/upload_fanart.php
@@ -8,7 +8,7 @@ $gestImg = new gestImg();
 $no_error = true;
 new Debugger("Gonna upload");
 new Debugger($_FILES['uploadImage']['size']);
-if($_FILES['uploadImage']['size'] != 0 && $_FILES['upload_image']['error'] == 0){
+if($_FILES['uploadImage']['size'] != 0 && $_FILES['uploadImage']['error'] == 0){
 	new Debugger("Uploading");
 	$uploadResult = $gestImg->uploadImage($target_dir,"uploadImage");
 	new Debugger( "Fine upload ");
