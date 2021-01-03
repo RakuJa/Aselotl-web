31c2590125e08b2bfafe5301d229c0184141445b
RakuJa <danielegiachetto1999@gmail.com>
2021-01-03 23:06:02 +0100

    Add delete_account.php

---
+++ php/delete_account.php
@@ -0,0 +1,28 @@
+<?php
+	require_once('connessione.php');
+	require_once('debugger.php');
+	require_once('sessione.php');
+	$no_error=true;
+	$obj_connection = new DBConnection();
+	if(!$obj_connection->create_connection()){
+        new Debugger("[Errore di connessione al database]");
+        $error=$error."<div class=\"msg_box error_box\">Errore di connessione al database</div>";
+        $no_error=false;
+    }
+
+	$email="";
+	if (!isset($_GET['email']) || $_SESSION['logged']==false || 
+		($_SESSION['PERMISSION']!=0 && $_SESSION['EMAIL'] != $_GET['email'])) {
+		header("location: ../php/access_denied.php");		
+	}else {
+		$email = $_GET['email'];
+		$query = "DELETE FROM User WHERE EMAIL = '$email'";
+		$no_error = !$obj_connection->insertDB($query);
+		if ($_SESSION['PERMISSION']==0) {
+			header("location: ../php/admin.php");
+		}else {
+			header("location: ../php/logout.php");
+		}
+
+	}
+?>	
