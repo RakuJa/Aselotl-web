8b9302a0492ff8e0bf2ae90d0adbe69e6dd97202
RakuJa <danielegiachetto1999@gmail.com>
2021-01-04 01:09:16 +0100

    Fix borked edit pwd

--- php/edit_password_check.php
+++ php/edit_password_check.php
@@ -2,7 +2,9 @@
 	require_once('connessione.php');
 	require_once('debugger.php');
 	require_once('sessione.php');
+	require_once("reg_ex.php");
 	$no_error=true;
+	$error="";
 	$obj_connection = new DBConnection();
 	if(!$obj_connection->create_connection()){
         new Debugger("[Errore di connessione al database]");
@@ -11,14 +13,11 @@
         exit();
         $no_error=false;
     }
-
 	$email="";
 	if (!isset($_POST['email']) || $_SESSION['logged']==false || 
-		!isset($_POST['pwd']) || !isset($_POST['pwd2']) ||
+		!isset($_POST['pwd']) || !isset($_POST['rpwd']) ||
 		($_SESSION['PERMISSION']!=0 && $_SESSION['EMAIL'] != $_POST['email'])) {
-		var_dump($_SESSION['EMAIL']);
-		var_dump($_POST['email']);
-		//header("location: ../php/access_denied.php");		
+		header("location: ../php/access_denied.php");		
 	}else {
 		$email = $_POST['email'];
 		$pwd = $_POST['pwd'];
@@ -27,10 +26,13 @@
     	$pwd = $obj_connection->escape_str(trim($pwd));
 		if ($pwd!=$pwd2) {
 			$error = $error."<div class=\"msg_box error_box\">Password e Ripeti Password non coincidono.</div>";
+			new Debugger("pwd non corrisponde");
 		}else {
 			if(!check_pwd($pwd)){
         		$error=$error."<div class=\"msg_box error_box\">La password deve essere lunga almeno 8 caratteri, contenere almeno una lettera maiuscola una minuscola e un numero.</div>";
+        		new Debugger("pwd non valida");
     		}else {
+    			new Debugger("preparo alla query");
 				$hashed_pwd=hash("sha512",$pwd);
 				$query = "UPDATE User SET PASSWORD = '$hashed_pwd' WHERE EMAIL = '$email'";
 				$no_error = !$obj_connection->insertDB($query);
@@ -43,4 +45,5 @@
 		}
 
 	}
+	new Debugger($error);
 ?>
