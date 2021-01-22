66203addb5c791535e3100863777eeecd6774ae2
Dema00 <dema199900@gmail.com>
2021-01-22 21:52:48 +0100

    Update header.php

--- php/header.php
+++ php/header.php
@@ -1,24 +1,25 @@
 <?php 
 	$protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
 	$url = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+	$host  = $_SERVER['HTTP_HOST'];
 	if (isset($_SESSION['logged']) && $_SESSION['logged']==true) {
-		if($url == "http://127.0.0.1/php/profile.php") {
+		if($url == "$protocol$host/php/profile.php") {
 			echo'<p> Profilo </p>';
 		} else {
 			echo'<a href="../php/profile.php" class="colored"> Profilo </a>';
 		}
 	echo'<a href="../php/logic/logout.php" xml:lang="en" lang="en" class="colored">Logout</a>';
 	} else {
-		if($url == "http://127.0.0.1/php/login.php") {
+		if($url == "$protocol$host/php/login.php") {
 			echo'<p> Accedi </p>';
 		} else {
 			echo'<a href="../php/login.php" class="colored"> Accedi </a>';
 		}
-		if($url == "http://127.0.0.1/php/register.php") {
+		if($url == "$protocol$host/php/register.php") {
 			echo'<p> Registrati </p>';
 		} else {
 			echo'<a href="../php/register.php" class="colored"> Registrati </a>';
 		}
-    } ?>
-</div>
-</div>
+	}
+	echo'</div> </div>';
+?>
