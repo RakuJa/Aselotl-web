d472beb3f72a2c531bdf6d86f31fec3ab4385e4c
RakuJa <danielegiachetto1999@gmail.com>
2021-01-23 01:26:19 +0100

    Clean up login

--- php/logic/login_check.php
+++ php/logic/login_check.php
@@ -57,41 +57,38 @@
             //$pwd = $obj_connection->escape_str(trim($pwd));
             $query = "SELECT * FROM user WHERE EMAIL = '$email' AND PASSWORD = '$hashed_pwd'";
             $permission = 2;
-            if($query_rist=$obj_connection->queryDB($query)){
-                $count=0;
+            $query_rist=$obj_connection->queryDB($query);
+            if($query_rist){
                 foreach ($query_rist as &$value) {
-                    $count=$count+1;
                     $permission = filter_var($value["PERMISSION"], FILTER_VALIDATE_BOOLEAN);
                 }
-                new Debugger($count);
-                if($count==0){
-                    $error="Le credenziali inserite non sono corrette<br />";
+                $_SESSION['logged']=true;
+                $_SESSION['EMAIL']=$email;
+                $_SESSION['PERMISSION']=$permission;
+                new Debugger("Logged in!");
+                if(isset($_POST['remember_me'])){
+                    setcookie("user_email",$email,time()+60*60*24*30,'/');
+                    setcookie("user_pwd",$pwd,time()+60*60*24*30,'/'); 
+                    new Debugger("Logged for a long time");   
                 }else{
-                    $_SESSION['logged']=true;
-                    $_SESSION['EMAIL']=$email;
-                    $_SESSION['PERMISSION']=$permission;
-                    new Debugger("Logged in!");
-                    if(isset($_POST['remember_me'])){
-                        setcookie("user_email",$email,time()+60*60*24*30,'/');
-                        setcookie("user_pwd",$pwd,time()+60*60*24*30,'/'); 
-                        new Debugger("Logged for a long time");   
-                    }else{
-                        setcookie("user_email","",time()-3600,'/');
-                        setcookie("user_pwd","",time()-3600,'/'); 
-                        new Debugger("Logged for a short while");
-                    }
-
-                    $obj_connection->close_connection();
-                
-                    header('location: ../index.php');
-                    exit;
+                    setcookie("user_email","",time()-3600,'/');
+                    setcookie("user_pwd","",time()-3600,'/'); 
+                    new Debugger("Logged for a short while");
                 }
-            }else {
-                new Debugger("query andata male");
-                $error="La query non è andata a buon fine";
-            }
-            $obj_connection->close_connection();
 
+                $obj_connection->close_connection();    
+                header('location: ../index.php');
+                exit;
+
+            }else {
+                if (is_null($query_rist)) {
+                    new Debugger("Le credenziali inserite non sono corrette");
+                    $error="Le credenziali inserite non sono corrette<br />";
+                }else {
+                    new Debugger("query andata male");
+                    $error="La query non è andata a buon fine<br />";
+                }
+            }
         }else{
             new Debugger("Connessione con il DB fallita");
             $error=(new errore('DBConnection'))->printHTMLerror();
