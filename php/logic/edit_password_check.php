7d8d778effe4cfb18c1815d37d2ba5cb0adb1d03
RakuJa <danielegiachetto1999@gmail.com>
2021-01-23 15:35:53 +0100

    Change error message

--- php/logic/edit_password_check.php
+++ php/logic/edit_password_check.php
@@ -8,7 +8,7 @@
 	$obj_connection = new DBConnection();
 	if(!$obj_connection->create_connection()){
         new Debugger("[Errore di connessione al database]");
-        $error=$error."<div class=\"msg_box error_box\">Errore di connessione al database</div>";
+        $error=$error."Errore di connessione al database<br />";
         header("location: ../php/index.php");
         exit();
         $no_error=false;
@@ -25,11 +25,11 @@
 		$email=$obj_connection->escape_str(trim($email));
     	$pwd = $obj_connection->escape_str(trim($pwd));
 		if ($pwd!=$pwd2) {
-			$error = $error."<div class=\"msg_box error_box\">Password e Ripeti Password non coincidono.</div>";
+			$error = $error."Password e Ripeti Password non coincidono.<br />";
 			new Debugger("pwd non corrisponde");
 		}else {
 			if(!check_pwd($pwd)){
-        		$error=$error."<div class=\"msg_box error_box\">La password deve essere lunga almeno 8 caratteri, contenere almeno una lettera maiuscola una minuscola e un numero.</div>";
+        		$error=$error."La password deve essere lunga almeno 8 caratteri, contenere almeno una lettera maiuscola una minuscola e un numero.<br />";
         		new Debugger("pwd non valida");
     		}else {
     			new Debugger("preparo alla query");
